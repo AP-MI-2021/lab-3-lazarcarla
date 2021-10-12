c8439dc2f36d79d9aa1e2b1e311ee97483600c69
lazarcarla <carlalazar@stud.ubbcluj.ro>
2021-10-12 10:18:31 +0300

    Carla Lazar lab3 problema 3,13,10

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def print_menu():
     print("1.Citire lista ")
     print("2.Afisare subsecventa cu cele mai multe numere cu semne alternante")
     print("3.Afisare subsecventa cu cele mai multe numere formate din cifre prime")
-    print("4.Iesire")
+    print("4.Afisare subsecventa cu cele mai multe numere formate din cifre pare")
+    print("5.Iesire")
 
 def get_longest_alternating_signs(l):
     '''
@@ -106,12 +107,47 @@ def test_subsecventa_maxima_cu_elemente_formate_din_cifre_prime():
     assert subsecventa_maxima_cu_elemente_formate_din_cifre_prime([3,35,6,7])==[3,35]
     assert subsecventa_maxima_cu_elemente_formate_din_cifre_prime([4,6])==[]
 
+def get_longest_all_even(l):
+    '''
+    determina daca toate numerele sunt pare intr-o lista
+    :param l: lista cu numerele intregi
+    :return: true daca lista este formata doar din numere pare
+    '''
+    for x in l:
+        if x%2!=0:
+            return False
+    return True
+
+def test_get_longest_all_even():
+    assert get_longest_all_even([2,4,6]) is True
+    assert get_longest_all_even([2,4,1,13]) is False
+
+def subsecventa_maxima_numere_pare(l):
+    '''
+    determina cea mai lunga subsecventa formata din numere pare
+    :param l: lista cu numere intregi
+    :return: subsecventa maxima formata din numere pare
+    '''
+    subMax=[]
+    for i in range(len(l)):
+        for j in range(i,len(l)):
+            if get_longest_all_even(l[i:j + 1]) and len(subMax) < len(l[i:j + 1]):
+                subMax=l[i:j + 1]
+    return subMax
+
+def test_subsecventa_maxima_numere_pare():
+    assert subsecventa_maxima_numere_pare([2,34,6,5,13])==[2,34,6]
+    assert subsecventa_maxima_numere_pare([3,56,4,2,7])==[56,4,2]
+
+
 def main():
     test_get_longest_alternating_signs()
     test_cea_mai_lunga_subsecventa_cu_numere_cu_semne_alternante()
     test_is_prime()
     test_get_longest_prime_digits()
     test_subsecventa_maxima_cu_elemente_formate_din_cifre_prime()
+    test_get_longest_all_even()
+    test_subsecventa_maxima_numere_pare()
     l=[]
     print_menu()
     while True:
@@ -124,6 +160,9 @@ def main():
 
         elif optiune =="3":
             print(subsecventa_maxima_cu_elemente_formate_din_cifre_prime(l))
+
+        elif optiune=="4":
+            print(subsecventa_maxima_numere_pare(l))
 
         else:
             print("optiune gresita ! reincercati!")
